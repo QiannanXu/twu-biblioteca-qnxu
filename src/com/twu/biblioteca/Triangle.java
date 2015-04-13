0ecd0e4dd752b2f0b5e7e252d8abde8dad4360cc
QiannanXu <qnxu@thoughtworks.com>
2015-04-13 22:27:04 +0800

    refactor and add IsoscelesTriangle, Diamond, DiamondWithName functions

--- src/com/twu/biblioteca/Triangle.java
+++ src/com/twu/biblioteca/Triangle.java
@@ -6,12 +6,20 @@ public class Triangle {
         return "*";
     }
 
-    public String printAHorizontalLine(int columnNum) {
-        String outputMessage = "";
-        for(int i = 0; i < columnNum; i++){
-            outputMessage += "*";
+    public String printAHorizontalLine(int n) {
+        String asteriskMessage = "";
+        for(int i = 0; i < n; i++){
+            asteriskMessage += "*";
         }
-        return outputMessage;
+        return asteriskMessage;
+    }
+
+    public String printNBlank(int n) {
+        String blankMessage = "";
+        for(int i = 0; i < n; i++){
+            blankMessage += " ";
+        }
+        return blankMessage;
     }
 
     public String printAVerticalLine(int rowNum) {
@@ -20,5 +28,45 @@ public class Triangle {
             outputMessage += "*\n";
         }
         return outputMessage;
+    }
+
+    public String drawARightTriangle(int n) {
+        String outputMessage = "";
+        for(int i = 0; i < n; i++){
+            outputMessage += printAHorizontalLine(i + 1) + "\n";
+        }
+        return outputMessage;
+    }
+
+    public String drawIsoscelesTriangle(int n) {
+        String outputMessage = "";
+        for(int i = 0; i < n; i++){
+            outputMessage += printNBlank(n-i-1) + printAHorizontalLine(2*i+1) + "\n";
+        }
+        return outputMessage;
+    }
+
+    private String drawReverseIsoscelesTriangle(int n) {
+        String outputMessage = "";
+        for(int i = n-1; i >= 0; i--){
+            outputMessage += printNBlank(n-i) + printAHorizontalLine(2*i+1) + "\n";
+        }
+        return outputMessage;
+    }
+
+    public String drawCenteredDiamond(int n) {
+        return drawIsoscelesTriangle(n) + drawReverseIsoscelesTriangle(n-1);
+    }
+
+    public String drawCenteredDiamondWithName(int n) {
+        return drawIsoscelesTriangleWithName(n) + drawReverseIsoscelesTriangle(n-1);
+    }
+
+    private String drawIsoscelesTriangleWithName(int n) {
+        String outputMessage = "";
+        for(int i = 0; i < n-1; i++){
+            outputMessage += printNBlank(n-i-1) + printAHorizontalLine(2*i+1) + "\n";
+        }
+        return outputMessage + "QiannanXu\n";
     }
 }
--- test/com/twu/biblioteca/TriangleTest.java
+++ test/com/twu/biblioteca/TriangleTest.java
@@ -1,22 +1,51 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
+
+    private Triangle triangle;
+
+    @Before
+    public void init(){
+        triangle = new Triangle();
+    }
+
     @Test
     public void shouldPrintOneAsteriskToConsole(){
-        assertEquals("*", new Triangle().printOneAsterisk());
+        assertEquals("*", triangle.printOneAsterisk());
     }
 
     @Test
     public void shouldPrintAHorizontalLineToConsole(){
-        assertEquals("********", new Triangle().printAHorizontalLine(8));
+        assertEquals("********", triangle.printAHorizontalLine(8));
     }
 
     @Test
     public void shouldPrintAVerticalLineToConsole(){
-        assertEquals("*\n*\n*\n", new Triangle().printAVerticalLine(3));
+        assertEquals("*\n*\n*\n", triangle.printAVerticalLine(3));
+    }
+
+    @Test
+    public void shouldDrawARightTriangle(){
+        assertEquals("*\n**\n***\n", triangle.drawARightTriangle(3));
+    }
+
+    @Test
+    public void shouldDrawIsoscelesTriangle(){
+        assertEquals("  *\n ***\n*****\n", triangle.drawIsoscelesTriangle(3));
+    }
+
+    @Test
+    public void shouldDrawCenteredDiamod(){
+        assertEquals("  *\n ***\n*****\n ***\n  *\n", triangle.drawCenteredDiamond(3));
+    }
+
+    @Test
+    public void shouldDrawCenteredDiamodWithName(){
+        assertEquals("  *\n ***\nQiannanXu\n ***\n  *\n", triangle.drawCenteredDiamondWithName(3));
     }
 }
