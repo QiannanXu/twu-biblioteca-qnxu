b5a9d5e1227d93658b32d9d1b765298ba711d056
QiannanXu <qnxu@thoughtworks.com>
2015-04-13 22:41:54 +0800

    add FizzBuzz unit test cases and fucntions

--- src/com/twu/biblioteca/FizzBuzz.java
+++ src/com/twu/biblioteca/FizzBuzz.java
@@ -2,7 +2,24 @@ package com.twu.biblioteca;
 
 public class FizzBuzz {
 
-    public String print() {
-        return "1";
+    public String print(int i) {
+        if(i%3 == 0 && i%5 != 0){
+            return "Fizz";
+        }
+        if(i%3 != 0 && i%5 == 0){
+            return "Buzz";
+        }
+        if(i%3 == 0 && i%5 == 0){
+            return "FizzBuzz";
+        }
+        return Integer.toString(i);
+    }
+
+    public String printNNumber(int n) {
+        String outputMessage = "";
+        for(int i=0; i<n; i++){
+            outputMessage += print(i+1) + "\n";
+        }
+        return outputMessage;
     }
 }
--- test/com/twu/biblioteca/FizzBuzzTest.java
+++ test/com/twu/biblioteca/FizzBuzzTest.java
@@ -7,6 +7,31 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void shouldPrintOneWhenNumberIsOne(){
-        assertEquals("1", new FizzBuzz().print());
+        assertEquals("1", new FizzBuzz().print(1));
+    }
+
+    @Test
+    public void shouldPrintTwoWhenNumberIsTwo(){
+        assertEquals("2", new FizzBuzz().print(2));
+    }
+
+    @Test
+    public void shouldPrintFizzWhenNumberIsThree() {
+        assertEquals("Fizz", new FizzBuzz().print(3));
+    }
+
+    @Test
+    public void shouldPrintBuzzWhenNumberIsFive(){
+        assertEquals("Buzz", new FizzBuzz().print(5));
+    }
+
+    @Test
+    public void shouldPrintFizzBuzzWhenNumberIsFifteen(){
+        assertEquals("FizzBuzz", new FizzBuzz().print(15));
+    }
+
+    @Test
+    public void shouldPrintCorrentMessageForNumberRange(){
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n", new FizzBuzz().printNNumber(15));
     }
 }
